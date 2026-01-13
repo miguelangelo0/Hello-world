ff825b1e44273c3577e0c489037318eec7176fa9
Miguel <midas.an75@gmail.com>
2026-01-13 02:20:33 +0000

    Login BASIC

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,27 @@
+ import java.util.Scanner;
  public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá , seja bem-vindo!");
+	
+	Scanner input = new Scanner(System.in);
+
+	String userCerto = "Exemplo";
+	String passwordCerta = "Exemplopswd";
+
+	System.out.println("user: ");
+	String user = input.nextLine();
+
+	System.out.println("password: ");
+	String password = input.nextLine();
+
+	if (user.equals(userCerto) && password.equals(passwordCerta)) {
+		System.out.println("Login efetuado com sucesso. Bem-vindo!");
+	} else {
+		System.out.println("User ou Password incorretos.");
+	}
+	input.close();
+
+
+
     }
 }
